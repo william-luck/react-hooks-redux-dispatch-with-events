cafce2c3154b04de94ab79601241eee2677fc7a9
William Luck <luck.william.h@gmail.com>
2023-03-24 10:14:09 -0300

    commit tree d3913b8efafbdfc0326791adddda415e4ccd1b82

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,33 @@
-// add code snippets from README
+let state;
+
+function reducer(state = {count: 0}, action) {
+    switch (action.type) {
+        case 'counter/increment' :
+            return {count: state.count + 1}
+        default: 
+            return state
+    }
+}
+
+function dispatch(action) {
+    state = reducer(state, action)
+    render()
+    
+}
+
+function render() {
+    const container = document.getElementById("container");
+    container.textContent = state.count;
+  }
+
+dispatch({type: '@@INIT'})
+
+const button = document.getElementById('button')
+
+
+// Vanilla JS for calling dispatch when the button is clicked. Nice
+button.addEventListener('click', () => {dispatch({type: 'counter/increment'})})
+
+
+
+
